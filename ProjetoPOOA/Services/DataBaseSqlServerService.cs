0ee70b326394b9316824cd9928009fef6b6cd87e
Lucas Alberto <lucas.alberto@linx.com.br>
2024-10-04 11:40:19 -0300

    Aula dia 03/10/2024
    Aula dia 03/10/2024

--- ProjetoPOOA/Services/DataBaseSqlServerService.cs
+++ ProjetoPOOA/Services/DataBaseSqlServerService.cs
@@ -20,6 +20,22 @@ namespace ProjetoPOOA.Services
                 "Integrated Security=SSPI;" + //Autenticação do Windows (usuario logado)
                 "User Instance=false;"; //Usar o usuario da maquina
 
+            //de String de comunicação para servidor Sql (com usuario e senha)
+            /*
+            conexao.ConnectionString =
+                "Data Source=192.168.0.5;" + //Servidor
+                "Initial Catalog=ProjetoPOOA;" + //Nome do Banco
+                "Persist Security Info=False;" + //Autenticação por usuario e senha
+                "User ID=usuario;" + //Usuario do login
+                "Password=senha;"; //Senha do login
+            */
+
+            //Variações:
+            //Servidor  local na rede
+            //"Data Source=192.168.0.5;" 
+            //Servidor online (1433 é a porta do banco de dados)
+            //"Data Source=tcp:estagio.database.windows.net,1433;" 
+
             conexao.Open(); //Abrir a conexão com o banco
             return conexao;
         }
